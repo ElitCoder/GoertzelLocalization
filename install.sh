cc099b32371267153ee5a1cca346a40c7bf66870
Niklas <niklas.carlsson1994@gmail.com>
2018-03-14 18:04:08 +0100

    Revert using prebuilt libssh and cleanup repo

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug -DWITH_STATIC_LIB=ON 
 make -j 9
 
 if [ $? -ne 0 ]; then
+	exit 1
+	
+	# this is dangerous
 	echo "Building failed, copy the prebuilts instead and hope for the best"
 	
 	cd ../../prebuilt/
