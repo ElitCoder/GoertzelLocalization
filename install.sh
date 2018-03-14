505b2d803df03c4ba3ab10ede6595e7f14e779bf
Niklas Carlsson <niklasc@lnxniklasc.se.axis.com>
2018-03-14 13:33:48 +0100

    Added prebuilt libssh

--- install.sh
+++ install.sh
@@ -5,8 +5,24 @@ unar libssh-0.7.5.tar.xz
 cd libssh-0.7.5/
 mkdir build
 cd build/
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug ..
-make -j 9
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug -DWITH_STATIC_LIB=ON ..
+#make -j 9
+
+if [ $? -ne 1 ]; then
+	echo "Building failed, copy the prebuilts instead and hope for the best"
+	
+	cd ../../prebuilt/
+	cd include/
+	echo "Copying include/libssh.. "
+	sudo cp -r libssh /usr/include
+	cd ../lib/
+	echo "Copying lib/.."
+	sudo cp * /usr/lib/
+	
+	exit 0
+fi
+
 sudo make install
 cd ../../
-exit
+
+exit 0
