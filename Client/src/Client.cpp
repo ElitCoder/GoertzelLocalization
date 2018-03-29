40a78d9169d361afabb0eaa6d97a3d47a6d6a472
test@test.com <test@test.com>
2018-03-29 10:30:21 +0200

    New IP and lower timeout
    * 30s -> 15s

--- Client/src/Client.cpp
+++ Client/src/Client.cpp
@@ -30,9 +30,9 @@ enum {
 static NetworkCommunication* g_network;
 
 // Speakers
-static vector<string> g_ips = { "172.25.14.27", "172.25.13.200" };
+static vector<string> g_ips = { "172.25.14.27", "172.25.11.186" };
 // External microphones
-static vector<string> g_external_microphones = { "172.25.9.38", "172.25.11.186" };
+static vector<string> g_external_microphones = { "172.25.9.38", "172.25.13.200", "172.25.11.47", "172.25.12.99" };
 
 Packet createSetSpeakerSettings(const vector<string>& ips, const vector<int>& volumes, const vector<int>& captures, const vector<int>& boosts) {
 	Packet packet;
