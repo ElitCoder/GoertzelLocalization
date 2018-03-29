cf7ed028c07753f290c6d45d8def3d4ce9f10cfd
Niklas Carlsson <niklasc@lnxniklasc.se.axis.com>
2018-03-29 10:57:36 +0200

    Added libcurlpp dependency
    * For sending HTTP request to enable SSH

--- dependencies/clean.sh
+++ dependencies/clean.sh
@@ -1,8 +1,13 @@
 #/bin/bash
 
+# libssh
 rm -rf libssh-0.7.5
 rm -rf XADTemp*
 
+# libcurlpp
+rm -rf curlpp-0.8.1/
+
+# libnessh
 cd nessh/
 make clean
 cd cli/
--- install.sh
+++ install.sh
@@ -24,6 +24,26 @@ function build_nessh {
 	sudo make install
 }
 
+function build_libssh {
+	unar libssh-0.7.5.tar.xz
+	cd libssh-0.7.5/
+	mkdir build
+	cd build/
+	cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug ..
+	make -j $cores
+	sudo make install
+	cd ../../
+}
+
+function build_curlpp {
+	unar curlpp-0.8.1.tar.gz
+	cd curlpp-0.8.1/ && mkdir build; cd build/
+	cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug ..
+	make -j $cores
+	sudo make install
+	cd ../../
+}
+
 cd dependencies/
 
 if [ $# -ne 0 ]; then
@@ -31,16 +51,17 @@ if [ $# -ne 0 ]; then
 		build_nessh
 	fi
 	
+	if [ $1 = "libssh" ]; then
+		build_libssh
+	fi	
+	
+	if [ $1 = "curlpp" ]; then
+		build_curlpp
+	fi
+	
 	exit 0
 fi 		
 
-unar libssh-0.7.5.tar.xz
-cd libssh-0.7.5/
-mkdir build
-cd build/
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug ..
-make -j $cores
-sudo make install
-cd ../../
-
+build_libssh
+build_curlpp
 build_nessh
